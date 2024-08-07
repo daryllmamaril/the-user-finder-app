a3127e9b47bfb7306bc049c5a800a6a5aaf8862b
daryll.mamaril <daryll.mamaril@avanade.com>
2024-08-08 00:31:37 +1000

    Add fetching api and add search filter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((user) => {
+          return value && user && user.name && user.name.toLowerCase().includes(value)
+        })
+        console.log(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
   return (
     <>
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-light bg-light searchbar-container">
         <form className="form-inline">
           <input
             className="form-control mr-sm-2"
             type="search"
-            placeholder="Search a user..."
+            placeholder="Type to find a user..."
             aria-label="Search"
+            value={input}
+            onChange={(e) => handleChange(e.target.value)}
           />
           <button className="btn btn-info my-2 my-sm-0" type="submit">
             Search
